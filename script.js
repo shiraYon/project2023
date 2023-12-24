f77b21be8fd7ae041259180ed32d1948c1e48172
shiraYon <154463479+shiraYon@users.noreply.github.com>
2023-12-24 22:57:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const defaultValues = {
     shrinkage: 40,
     thickness_US: 0,
   };
-  
+
   // Set default values to input elements
   Object.keys(defaultValues).forEach((key) => {
     const inputElement = document.getElementById(key);
@@ -16,7 +16,8 @@ const defaultValues = {
       inputElement.value = defaultValues[key];
     }
   });
-  
+
+
   function calculateSize() {
     // Get user input
     const height = parseFloat(document.getElementById("height").value);
@@ -29,7 +30,7 @@ const defaultValues = {
       document.getElementById("thickness_US").value,
     );
     const radiation = document.getElementById("radiation").checked || false;
-  
+
     // Validate input
     if (
       isNaN(height) ||
@@ -41,52 +42,54 @@ const defaultValues = {
       alert("Please enter valid numerical values.");
       return;
     }
-  
+
     // Update shrinkage based on radiation
     const finalShrinkage = radiation ? 50 : shrinkagePercentage;
-  
+
     // Calculate new size
     const shrinkageFactor = 1 - finalShrinkage / 100;
     const newHeight = (height + margin) / shrinkageFactor;
     const newWidth = (width + margin) / shrinkageFactor;
-  
+
     // Calculate corrected thickness
     const correctedThickness = thickness_US * 0.7;
-  
+
     // Display results in the result div
     const resultDiv = document.getElementById("result");
-    resultDiv.innerHTML = ` Tumor Size:<br>Height: ${height} cm, Width: ${width} cm<br><br><strong>The size of the tissue that needs to be cut:<br>Height: ${newHeight.toFixed(
+    resultDiv.innerHTML = `* Original tumor size:<br>Height: ${height} cm, Width: ${width} cm<br><br>
+    * Tumor size with margins:<br>Height: ${height+margin} cm, Width: ${width+margin} cm<br><br>
+    <strong>* With a  ${finalShrinkage}% shrinkage you need to cut tissue of size:<br>Height: ${newHeight.toFixed(
       2,
     )} cm, Width: ${newWidth.toFixed(
       2,
-    )} cm</strong><br><br>Thickness after correction of 30%: ${correctedThickness.toFixed(
+    )} cm</strong><br><br>* Thickness after correction of 30%: ${correctedThickness.toFixed(
       2,
     )} cm`;
-  
+
     if (radiation) {
       resultDiv.innerHTML +=
-        "<br><br>Since radiation is involved<br>we used shrinkage of 50%.";
+        "<br><br>* Since radiation is involved<br>we used shrinkage of 50%.";
     }
-  
+
     // Draw ellipse
     const canvas = document.getElementById("ellipseCanvas");
     const ctx = canvas.getContext("2d");
-  
+
     // Convert centimeters to pixels
     const cmToPixels = 20;
-  
+
     const ellipseWidth = newWidth * cmToPixels;
     const ellipseHeight = newHeight * cmToPixels;
-  
+
     // Set canvas size based on ellipse dimensions
     canvas.width = ellipseWidth +30 ;
     canvas.height = ellipseHeight +30;
-  
+
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
     // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+
     // Draw rectangle
     ctx.beginPath();
     ctx.rect(
@@ -97,7 +100,7 @@ const defaultValues = {
     );
     ctx.stroke();
     ctx.closePath();
-  
+
     // Draw ellipse
     ctx.beginPath();
     ctx.ellipse(
@@ -114,16 +117,16 @@ const defaultValues = {
     ctx.strokeStyle = "black";
     ctx.stroke();
     ctx.closePath();
-  
-  
+
+
     // Draw vertical line (אנכי)
-    ctx.strokeStyle = "blue";
+    ctx.strokeStyle = "black";
     ctx.beginPath();
     ctx.moveTo(centerX, centerY - ellipseHeight / 2);
     ctx.lineTo(centerX, centerY + ellipseHeight / 2);
     ctx.stroke();
     ctx.closePath();
-  
+
     // Draw horizontal line (אופקי)
     ctx.beginPath();
     ctx.moveTo(centerX - ellipseWidth / 2, centerY);
@@ -131,9 +134,8 @@ const defaultValues = {
     ctx.stroke();
     ctx.closePath();
     // Display dimensions on the canvas
-      ctx.fillStyle = "blue";
+      ctx.fillStyle = "black";
       ctx.font = "12px Arial";
-      ctx.fillText(`width: ${newWidth.toFixed(2)} cm`, centerX + ellipseWidth / 32, centerY - 7);
-      ctx.fillText(`height: ${newHeight.toFixed(2)} cm`, centerX - 90, centerY + ellipseWidth / 32+50);
+      ctx.fillText(`width: ${newWidth.toFixed(2)}`, centerX + ellipseWidth / 32, centerY - 7);
+      ctx.fillText(`height: ${newHeight.toFixed(2)}`, centerX - 75, centerY + ellipseWidth / 32+50);
     }
-   
